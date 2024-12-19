34f34422ba0acef21990b9cdf4e9c6c8d7de7141
akufarish <risfarishk@gmail.com>
2024-12-19 08:55:16 +0800

    background image and move character with mouse event

--- app.c
+++ app.c
@@ -6,11 +6,12 @@
 
 #define COLS 20
 #define ROWS 20
-#define WINDOW_TITLE "Snake Game"
-#define WINDOW_WIDTH  800
-#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE "Space Rocket"
+#define WINDOW_WIDTH  600
+#define WINDOW_HEIGHT 800
 #define BLOCK_SIZE 25;
 #define PESAWAT_PATH "res/img/pesawat.png"
+#define BACKGROUND_IMG "res/img/bg.png"
 
 struct Game
 {
@@ -29,22 +30,25 @@ struct Karakter
 };
 
 
+
+
 bool sdl_initialize(struct Game *game);
 void karakter_initialize(struct Karakter *karakter, SDL_Renderer *renderer);
 void generateAmmo(struct Karakter *karakter, struct Karakter *peluru, SDL_Renderer *renderer);
 void game_cleanup(struct Game *game, int exit_status);
 
 int main(int argc, char** argv) {
+
     struct Game game = {
         .window = NULL,
         .renderer = NULL
     };
 
     struct Karakter karakter = {
-        .h = 10,
-        .w = 10,
-        .x = 50,
-        .y = 50,
+        .h = 100,
+        .w = 100,
+        .x = 10,
+        .y = 10,
         .velocity_x = 0,
         .velocity_y = 0
     };
@@ -68,9 +72,7 @@ int main(int argc, char** argv) {
         bool isAmmoActive = false; 
 
 while (true) {
-    SDL_Delay(200);
-    peluru.y += peluru.velocity_y * BLOCK_SIZE;
-
+    SDL_ShowCursor(SDL_DISABLE);
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
@@ -89,18 +91,13 @@ while (true) {
             case SDL_MOUSEMOTION:
                 int x, y;
                 SDL_GetMouseState(&x, &y);
-                karakter.x = x;
-                karakter.y = y;
+                karakter.x = x - karakter.w / 5;
+                karakter.y = y - karakter.h / 5;
                 break;
             case SDL_MOUSEBUTTONDOWN:
                 switch (event.button.button) {
                     case SDL_BUTTON_LEFT:
                         printf("Klik kiri\n");
-                        isAmmoActive = true; 
-                        peluru.x = karakter.x + karakter.w / 2 - peluru.w / 2; 
-                        peluru.y = karakter.y - peluru.h; 
-                        peluru.velocity_x = 0;
-                        peluru.velocity_y = -1;
                         break;
                     default:
                         break;
@@ -112,14 +109,14 @@ while (true) {
     }
 
     // Clear screen
-    SDL_SetRenderDrawColor(game.renderer, 255, 255, 255, 255);
+    SDL_Texture *bg = IMG_LoadTexture(game.renderer, BACKGROUND_IMG);
+    // SDL_SetRenderDrawColor(game.renderer, 255, 255, 255, 255);
     SDL_RenderClear(game.renderer);
+    SDL_RenderCopy(game.renderer, bg, NULL, NULL);
 
     karakter_initialize(&karakter, game.renderer);
 
-    if (isAmmoActive) {
-        generateAmmo(&karakter, &peluru, game.renderer);
-    }
+   
 
     gameOver(&karakter, &game);
 
@@ -135,9 +132,18 @@ while (true) {
 
 
 void gameOver(struct Karakter *karakter, struct Game *game) {
-    if (karakter->x < 0 || karakter->x >= WINDOW_WIDTH || karakter->y < 0 || karakter->y >= WINDOW_HEIGHT) {
-        game_cleanup(game, EXIT_SUCCESS);
+  if (karakter->x < 0) {
+        karakter->x = 0; 
+    } else if (karakter->x > WINDOW_WIDTH - 100) { 
+        karakter->x = WINDOW_WIDTH - 100; 
     }
+
+    if (karakter->y < 0) {
+        karakter->y = 0; 
+    } else if (karakter->y > WINDOW_HEIGHT - 100) { 
+        karakter->y = WINDOW_HEIGHT - 100; 
+    }
+
 }
 
 
@@ -147,8 +153,8 @@ void karakter_initialize(struct Karakter *karakter, SDL_Renderer *renderer) {
     SDL_Texture *img = IMG_LoadTexture(renderer, PESAWAT_PATH);
     SDL_QueryTexture(img, NULL, NULL, &karakter->w, &karakter->h);
 
-    rect.h = karakter->h;
-    rect.w = karakter->w;
+    rect.h = karakter->h - 100;
+    rect.w = karakter->w - 100;
     rect.x = karakter->x;
     rect.y = karakter->y;
 
